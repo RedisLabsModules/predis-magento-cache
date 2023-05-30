299a5e908e3b314f388268bb720ace2106621f7b
Vladyslav Vildanov <divinez122@outlook.com>
2023-05-30 10:17:30 +0300

    Added missing test coverage

--- tests/Unit/ClientFactoryTest.php
+++ tests/Unit/ClientFactoryTest.php
@@ -20,6 +20,48 @@ class ClientFactoryTest extends TestCase
         $this->assertInstanceOf(Client::class, $factory->create($options));
     }
 
+    /**
+     * @return void
+     * @throws \Zend_Cache_Exception
+     */
+    public function testCreateClientThrowsExceptionOnMissingReplicationConfiguration(): void
+    {
+        $factory = new ClientFactory();
+
+        $this->expectException(\Zend_Exception::class);
+        $this->expectExceptionMessage('Replication configuration should be specified as array');
+
+        $factory->create(['replication' => 'missing']);
+    }
+
+    /**
+     * @return void
+     * @throws \Zend_Cache_Exception
+     */
+    public function testCreateClientThrowsExceptionOnMissingClusterConfiguration(): void
+    {
+        $factory = new ClientFactory();
+
+        $this->expectException(\Zend_Exception::class);
+        $this->expectExceptionMessage('Cluster configuration should be specified as array');
+
+        $factory->create(['cluster' => 'missing']);
+    }
+
+    /**
+     * @return void
+     * @throws \Zend_Cache_Exception
+     */
+    public function testCreateClientThrowsExceptionOnMissingConfiguration(): void
+    {
+        $factory = new ClientFactory();
+
+        $this->expectException(\Zend_Exception::class);
+        $this->expectExceptionMessage('Unknown connection type.');
+
+        $factory->create([]);
+    }
+
     public function optionsProvider(): array
     {
         return [
@@ -35,6 +77,7 @@ class ClientFactoryTest extends TestCase
                         [
                             'server' => 'test',
                             'port' => 6379,
+                            'role' => 'master',
                         ],
                     ],
                 ]],
@@ -45,6 +88,7 @@ class ClientFactoryTest extends TestCase
                         [
                             'server' => 'test',
                             'port' => 6379,
+                            'alias' => 'first',
                         ],
                     ],
                 ]],
